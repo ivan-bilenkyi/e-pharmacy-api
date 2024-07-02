4d53a6bde02ea818d2177d72ce42cebff38033be
ivanbilenkyi <ivanbilenky@gmail.com>
2024-07-02 15:26:34 +0300

    update register controller

--- controlers/auth.js
+++ controlers/auth.js
@@ -1,44 +1,48 @@
-import { v4 } from 'uuid'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import { HttpError } from '../helpers/HttpError.js'
-import {User} from "../db/user.js";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { HttpError } from '../helpers/HttpError.js';
+import { User } from '../db/user.js';
 
 export const register = async (req, res) => {
-    const { email, password } = req.body;
+    const { name, email, password } = req.body;
     const user = await User.findOne({ email });
 
     if (user) {
-        throw HttpError(409, "Email in use");
+        throw HttpError(409, "Email already in use")
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = {
+        name,
+        email,
+        password: hashedPassword
     }
 
-    const hashedPassword = bcrypt.hashSync(password, 10);
-    const verificationToken = v4();
+    const savedUser = await User.create(newUser);
 
-    const newUser = new User({
-        ...req.body,
-        password: hashedPassword,
-        verificationToken: verificationToken,
-    });
+    const payload = {
+        id: savedUser._id,
+    };
+    const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: "23h" });
 
-    await newUser.save();
-
-    const token = jwt.sign({ id: newUser._id }, process.env.SECRET_KEY, {
-        expiresIn: "12h",
-    });
+    const userNew = await User.findByIdAndUpdate(savedUser._id,{
+        dailyExerciseTime: 0,
+        dailyCalorie: 0,
+        token
+    })
 
     res.status(201).json({
         token,
         user: {
-            name: newUser.name,
-            email: newUser.email,
+            name: userNew.name,
+            email: userNew.email,
         }
-    });
-};
+    })
+}
 
 export const login = async (req, res, next) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
+
     if (!user) {
         throw HttpError(401, "Email is wrong")
     }
@@ -71,13 +75,13 @@ export const refreshUser = async (req, res) => {
     res.json({
         _id,
         name,
-        email
-    })
-}
+        email,
+    });
+};
 
 export const logout = async (req, res) => {
     const { _id } = req.user;
-    await User.findByIdAndUpdate(_id, { token: "" });
+    await User.findByIdAndUpdate(_id, { token: '' });
 
-    res.status(200).json({ message: "Logout success" });
-}
+    res.status(200).json({ message: 'Logout success' });
+};
